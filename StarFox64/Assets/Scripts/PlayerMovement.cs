0dc52416d801ffc1fa4d614ed4c6a09e6b8142c9
polmonroig <polmonroig@gmail.com>
2020-05-05 13:35:18 +0200

    Add basic movement and player gravity

--- StarFox64/Assets/Scripts/PlayerMovement.cs
+++ StarFox64/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+public class PlayerMovement : MonoBehaviour {
+
+
+    [SerializeField] private Transform target;
+
+    private CharacterController _controller;
+    
+    public float rotSpeed = 15.0f;
+    public float movementSpeed = 15.0f;
+    public float gravity = -2.0f; 
+
+    private void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+
+        Vector3 movement = Vector3.zero;
+        float horInput = Input.GetAxis("Horizontal");
+        float vertInput = Input.GetAxis("Vertical");
+        if (horInput != 0 || vertInput != 0)
+        {
+            movement.x = horInput * movementSpeed;
+            movement.y = vertInput * movementSpeed;
+            movement = Vector3.ClampMagnitude(movement, movementSpeed);
+            Quaternion tmp = target.rotation;
+            target.eulerAngles = new Vector3(0, target.eulerAngles.y, 0);
+            movement = target.TransformDirection(movement);
+            target.rotation = tmp;
+            Quaternion direction = Quaternion.LookRotation(movement);
+            transform.rotation = Quaternion.Lerp(transform.rotation, direction, rotSpeed * Time.deltaTime);
+        }
+
+        if (vertInput == 0)
+        {
+            movement.y = gravity; 
+        }
+        Debug.Log(Vector3.forward);
+        movement *= Time.deltaTime;
+        _controller.Move(movement);
+
+
     }
 }
