a95fc7276de1bfb0d69e7adf60af7b57e51056cb
polmonroig <polmonroig@gmail.com>
2020-05-16 08:45:05 +0200

    Add spaceship laser

---
+++ StarFox64/Assets/Scripts/ShootLaser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootLaser : MonoBehaviour {
+
+
+    private KeyCode shootCode = KeyCode.L; 
+    [SerializeField] private GameObject laser;
+    [SerializeField] private GameObject target;
+    public float force = 20.0f; 
+    
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+        if (Input.GetKeyDown(shootCode)) {
+            // instantiate a new laser at current position 
+            var outLaser = Instantiate(laser, target.transform.position, Quaternion.identity);
+            // must have a rigid body 
+            // apply a force to the laser
+            var rigidBody =  outLaser.GetComponent<Rigidbody>();
+            rigidBody.AddForce(target.transform.forward * force);
+        }
+    }
+}
