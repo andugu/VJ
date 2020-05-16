8303b93837e873f989a27537dea53add2b1fdaab
polmonroig <polmonroig@gmail.com>
2020-05-16 12:06:36 +0200

    Add fire particles to spaceship

---
+++ StarFox64/Assets/Scripts/RollMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollMovement : MonoBehaviour {
+
+
+    private KeyCode rollKey = KeyCode.R;
+    
+    
+    void Update() {
+        if (Input.GetKeyDown(rollKey)) {
+                     
+        }
+    }
+}
