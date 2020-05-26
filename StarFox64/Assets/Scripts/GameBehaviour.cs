b049df27e1ca732f59f7535cc6f00215e1f34ae4
polmonroig <polmonroig@gmail.com>
2020-05-26 21:42:54 +0200

    Add level management(win, lose) and god mode

---
+++ StarFox64/Assets/Scripts/GameBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameBehaviour : MonoBehaviour {
+
+    public string currentLevel; 
+    public string nextLevel; 
+    
+    public void WinGame()
+    {
+        SceneManager.LoadScene(nextLevel);
+    }
+    
+    public void LoseGame() {
+        SceneManager.LoadScene(currentLevel);
+    }
+   
+}
